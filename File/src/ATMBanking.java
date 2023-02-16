bb8da3ae109c9c21e799e8b6a0321e34bfec4d27
Kriddanai Berkbandee <zazac147000@gmail.com>
2023-02-16 11:55:12 +0700

    16-02-2023

---
+++ File/src/ATMBanking.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class ATMBanking {
+	private int money;
+	
+	static void main(String[] args) throws IOException{
+		
+		String temp = "";
+		while() {
+		Scanner num = new Scanner(System.in);
+		System.out.print("Enter account number  : ");
+	    String ac = num.nextLine();
+		
+		}
+  }
+}
